221b20cc31e03c1c18ba80f0761e5d254c7a74ed
FedDawb <122037269+FedDawb@users.noreply.github.com>
2023-12-13 19:17:07 +0000

    Added time update to JS seconds and milliseconds

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,40 +1,42 @@
-// London
-let londonElement = document.querySelector("#london");
-let londonDateElemnet = londonElement.querySelector(".date");
-let londonTimeElemnet = londonElement.querySelector(".time");
-let londonTime = moment().tz("Europe/London");
+function updateTime() {
+    // London
+    let londonElement = document.querySelector("#london");
+    let londonDateElemnet = londonElement.querySelector(".date");
+    let londonTimeElemnet = londonElement.querySelector(".time");
+    let londonTime = moment().tz("Europe/London");
+
+    londonDateElemnet.innerHTML = londonTime.format("MMMM Do YYYY");
+    londonTimeElemnet.innerHTML = londonTime.format(
+        "h:mm:ss:SS [<small>]A[</small>]"
+    );
+
+
+    // Milan
+    let milanElement = document.querySelector("#milan");
+    let milanDateElemnet = milanElement.querySelector(".date");
+    let milanTimeElemnet = milanElement.querySelector(".time");
+    let milanTime = moment().tz("Europe/Rome");
+
+    milanDateElemnet.innerHTML = milanTime.format("MMMM Do YYYY");
+    milanTimeElemnet.innerHTML = milanTime.format(
+        "h:mm:ss:SS [<small>]A[</small>]"
+    );
 
 
 
-londonDateElemnet.innerHTML = londonTime.format("MMMM Do YYYY");
-londonTimeElemnet.innerHTML = londonTime.format(
-"h:mm:ss [<small>]A[</small>]"
-);
-
-
-// Milan
-let milanElement = document.querySelector("#milan");
-let milanDateElemnet = milanElement.querySelector(".date");
-let milanTimeElemnet = milanElement.querySelector(".time");
-let milanTime = moment().tz("Europe/Milan");
+    // New York
+    let newYorkElement = document.querySelector("#new-york");
+    let newYorkDateElemnet = newYorkElement.querySelector(".date");
+    let newYorkTimeElemnet = newYorkElement.querySelector(".time");
+    let newYorkTime = moment().tz("America/New_York");
 
 
 
-milanDateElemnet.innerHTML = milanTime.format("MMMM Do YYYY");
-milanTimeElemnet.innerHTML = milanTime.format(
-"h:mm:ss [<small>]A[</small>]"
-);
+    newYorkDateElemnet.innerHTML = newYorkTime.format("MMMM Do YYYY");
+    newYorkTimeElemnet.innerHTML = newYorkTime.format(
+        "h:mm:ss:SS [<small>]A[</small>]"
+    );
+}
 
-
-// New York
-let newYorkElement = document.querySelector("#new-york");
-let newYorkDateElemnet = newYorkElement.querySelector(".date");
-let newYorkTimeElemnet = newYorkElement.querySelector(".time");
-let newYorkTime = moment().tz("America/New_York");
-
-
-
-newYorkDateElemnet.innerHTML = newYorkTime.format("MMMM Do YYYY");
-newYorkTimeElemnet.innerHTML = newYorkTime.format(
-"h:mm:ss [<small>]A[</small>]"
-);
+updateTime();
+setInterval(updateTime, 1);
